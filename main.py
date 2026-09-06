a5eba94831f61f4c9b4251a9eccc73f043c8067f
SHIN-K51 <s2321689@u.tsukuba.ac.jp>
2026-09-06 21:35:05 +0900

    Save local changes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from scrape import scrape_latest_codes
 from target import target
 from notification import send
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1545046400562241558/g5Co1qVAN-jMbEh4toQ-dqkUIzlmr3N_keAvX5VWbcd4HGIQDeei48_18t-Zqpdrt2k4"
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 def main():
     posts = scrape_latest_codes()
