e9f6770c561f6b0cd5dc8508a93155a16ce00fbc
SHIN-K51 <s2321689@u.tsukuba.ac.jp>
2026-09-19 14:58:33 +0900

    change targets

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import os
-from dotenv import load_dotenv
 from scrape import scrape_latest_codes
-from target import target
+from target import target, get_users
 from notification import send
 from insert import save_post
 
-load_dotenv()
-
-WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-
 def main():
+    users = get_users()
     posts = scrape_latest_codes()
 
     for i in posts:
-        if save_post(i) and target(i):
-            send(WEBHOOK_URL, i)
+        if save_post(i):         
+            for user in users:
+                user_id = user["user_id"]
+
+                if target(i, user_id):
+                    send(user["discord_webhook_url"], i)
 
 if __name__ == "__main__":
     main()
--- target.py
+++ target.py
@@ -1,13 +1,26 @@
-TARGET_VIVILLON = {
-    "SS",  # Sandstorm
-    "TD",  # Tundra
-    "IS",  # Icy Snow
-    "PL",  # Polar
-    "RV",  # River
-    "SN",  # Sun
-    "OC",  # Ocean
-    "AP"   # Archipelago
-}
+from supabase_client import supabase
 
-def target(post):
-    return post["vivillon"] in TARGET_VIVILLON
+def get_users():
+    response = (
+        supabase
+        .table("profiles")
+        .select("user_id, discord_webhook_url")
+        .execute()
+    )
+
+    return response.data
+
+def get_target_vivillons(user_id):
+    response = (
+        supabase.table("target_vivillons")
+        .select("vivillon_code")
+        .eq("user_id", user_id)
+        .execute()
+    )
+
+    return {row["vivillon_code"] for row in response.data}
+
+def target(post, user_id):
+    target_vivillons = get_target_vivillons(user_id)
+
+    return post["vivillon"] in target_vivillons
